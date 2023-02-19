ce5045be46d9b166cd46f50772033d52d53114d0
Jo4D <yusufhasan182@gmail.com>
2023-02-19 06:40:17 +0200

    singelton implementation

---
+++ SingeltonDesignPatternDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingeltonDesignPatternDemo
+{
+    public class Program
+    {
+        static TableServers Host1List = TableServers.GetTableServers();
+        static TableServers Host2List = TableServers.GetTableServers();
+
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Host1GetNextServer();
+                Host2GetNextServer();
+            }
+        }
+        private static void Host1GetNextServer()
+        {
+            Console.WriteLine("The Next Server Is " + Host1List.GetTheNextServer());
+        }
+        private static void Host2GetNextServer()
+        {
+            Console.WriteLine("The Next Server Is " + Host2List.GetTheNextServer());
+        }
+    }
+}
---
+++ SingeltonDesignPatternDemo/TableServers.cs
@@ -0,0 +1,35 @@
+﻿namespace SingeltonDesignPatternDemo
+{
+    public class TableServers
+    {
+        //static because that every instance of the Singleton type will use the same instance
+        //readonly because that you can't overwrite this with something else .. once it is assigned it can't be assigned to a new value.
+        private static readonly TableServers tableServers = new TableServers();
+        private List<string> Servers = new();
+        private int NextServer = 0;
+
+        //Private Constructor means that only things inside this  class can instantiate this class
+        private TableServers()
+        {
+            Servers.Add("Yusuf");
+            Servers.Add("Ahmed");
+            Servers.Add("Ibrahim");
+            Servers.Add("Refat");
+        }
+        // to return the same instance
+        public static TableServers GetTableServers()
+        {
+            return tableServers;
+        }
+        public string GetTheNextServer()
+        {
+            string output = Servers[NextServer];
+            NextServer += 1;
+            if (NextServer >= Servers.Count)
+            {
+                NextServer = 0;
+            }
+            return output;
+        }
+    }
+}
